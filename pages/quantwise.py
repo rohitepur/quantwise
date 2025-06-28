f7db7128e1072ef6b8249009e449e404bf998e11
Rohit epur <rohitepur@mac.lan>
2025-06-28 18:09:51 -0400

    cleanup

--- pages/quantwise.py
+++ pages/quantwise.py
@@ -75,14 +75,7 @@ def detect_event_days_single(data, ticker, return_window=20, volume_multiplier=1
     return df
 
 def prep_data(tickers):
-    data = yf.download(
-    tickers="AAPL",
-    start="2018-01-01",
-    interval="1d",
-    group_by="ticker",
-    progress=False
-    )
-   # data = yf.download(tickers, start="2018-01-01", group_by='ticker')
+    data = yf.download(tickers, start="2018-01-01", interval="1d", group_by='ticker', progress=False)
     st.write("YFinance download:", data.shape)
     all_event_days = []
     for ticker in data.columns.levels[0]:
